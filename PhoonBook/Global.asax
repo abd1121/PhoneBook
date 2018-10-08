c8d5ca077c0d4c2e251e173276b0aa5eb7ce234f
Abdullah <38455373+abd1121@users.noreply.github.com>
2018-10-08 18:55:12 +0500

    Entity models

---
+++ PhoonBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhoonBook.MvcApplication" Language="C#" %>
---
+++ PhoonBook/Models/AspNetUsers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoonBook.Models
+{
+    public class AspNetUsers
+    {
+    }
+}
