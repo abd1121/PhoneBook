885a6192535ab0a46afd2ecb8c8aec0e18f5a7d0
Abdullah <38455373+abd1121@users.noreply.github.com>
2018-10-08 23:47:13 +0500

    ContactsController contactviewmodel

---
+++ PhoonBook/Models/contactviewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoonBook.Models
+{
+    public class contactviewmodel
+    {
+        public int ContactId { get; set; }
+        public string ContactNumber { get; set; }
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+    }
+}
