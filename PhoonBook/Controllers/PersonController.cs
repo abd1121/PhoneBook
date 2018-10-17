0a515fbc2473201f12493ad99ddad104e0dcc0b9
iqra <ishfaqiqra5@gmail.com>
2018-10-17 14:45:09 +0500

    Editted persons and No of birthdays

--- PhoonBook/Controllers/PersonController.cs
+++ PhoonBook/Controllers/PersonController.cs
@@ -1,62 +1,103 @@
-﻿using Microsoft.AspNet.Identity;
-using PhoonBook.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
-
-namespace PhoonBook.Controllers
+using Microsoft.AspNet.Identity;
+using PhoneBook2.Models;
+namespace PhoneBook2.Controllers
 {
+
     [Authorize]
     public class PersonController : Controller
     {
         // GET: Person
         public ActionResult Index()
         {
-            /*
-
+            string id = User.Identity.GetUserId().ToString();
             PhoneBookDbEntities db = new PhoneBookDbEntities();
-            var list = db.Person.ToList();
-            List<personviewmodel> viewList = new List<personviewmodel>();
-            foreach (var s in list)
+            List<Person> list = db.People.ToList();
+            List<PersonViewModel> ViewList = new List<PersonViewModel>();
+            foreach (Person p in list)
             {
-                personviewmodel obj = new personviewmodel();
-                obj.AddedOn = s.AddedOn;
-                obj.UpdateOn = Convert.ToDateTime(s.UpdateOn);
-                obj.FirstName = s.FirstName;
-                obj.LastName = s.LastName;
-                obj.EmailId = s.EmailId;
-                obj.HomeCity = s.HomeCity;
-                viewList.Add(obj);
-                
-            }*/
+                if (p.AddedBy == id)
+                {
+                    PersonViewModel obj = new PersonViewModel();
+                    obj.PersonId = p.PersonId;
+                    obj.FirstName = p.FirstName;
+                    obj.MiddleName = p.MiddleName;
+                    obj.LastName = p.LastName;
+                    obj.AddedBy = p.AddedBy;
+                    obj.AddedOn = p.AddedOn;
+                    obj.DateOfBirth = p.DateOfBirth;
+                    obj.EmailId = p.EmailId;
+                    obj.FaceBookAccountId = p.FaceBookAccountId;
+                    obj.HomeAddress = p.HomeAddress;
+                    obj.LinkedInId = p.LinkedInId;
+                    obj.ImagePath = p.ImagePath;
+                    obj.TwitterId = p.TwitterId;
+                    obj.UpdateOn = p.UpdateOn;
+                    obj.HomeCity = p.HomeCity;
+                    ViewList.Add(obj);
+                }
+               
+            }
+            return View(ViewList);
+        }
+         
 
-            PhoneBookDbEntities db = new PhoneBookDbEntities();
+        // GET: Person
+        public ActionResult TotalPersons()
+        {
             int s = 0;
-            string user_id = User.Identity.GetUserId().ToString();
-            var persons = db.Person.Where(x=>x.AddedBy==user_id);
-            List<Person> pr = new List<Person>();
-            foreach(var i in persons)
+            string id = User.Identity.GetUserId().ToString();
+            PhoneBookDbEntities db = new PhoneBookDbEntities();
+            List<Person> list = db.People.ToList();
+            foreach (Person p in list)
             {
-                Person p = new Person();
-                p.PersonId = i.PersonId;
-                p.FirstName = i.FirstName;
-                p.MiddleName = i.MiddleName;
-                p.LastName = i.LastName;
-                pr.Add(p);
-                s = s + 1;
+                if (p.AddedBy == id)
+                {
+                    s = s + 1;
+                }
+               
+            }
+            return Content(s.ToString());
+        }
+        public ActionResult UpcomingBirthdays()
+        {
+            
+            string id = User.Identity.GetUserId().ToString();
+            PhoneBookDbEntities db = new PhoneBookDbEntities();
+            var list = db.People.Where(x => x.AddedBy == id);
+            List<Person> ViewList = new List<Person>();
+            foreach (var p in list)
+            {
+                    if (p.DateOfBirth.Value.Day == DateTime.Now.Day && p.DateOfBirth.Value.Month == DateTime.Now.Month ||
+                        p.DateOfBirth.Value.Day == DateTime.Now.AddDays(1).Day && p.DateOfBirth.Value.Month == DateTime.Now.Month)
+                    {
+                        Person obj = new Person();
+                        obj.PersonId = p.PersonId;
+                        obj.FirstName = p.FirstName;
+                        obj.MiddleName = p.MiddleName;
+                        obj.LastName = p.LastName;
+                        obj.AddedBy = p.AddedBy;
+                        obj.DateOfBirth = p.DateOfBirth;
+                        obj.EmailId = p.EmailId;
+                        obj.FaceBookAccountId = p.FaceBookAccountId;
+                        obj.HomeAddress = p.HomeAddress;
+                        obj.LinkedInId = p.LinkedInId;
+                        obj.ImagePath = p.ImagePath;
+                        obj.TwitterId = p.TwitterId;
+                        obj.UpdateOn = p.UpdateOn;
+                        obj.HomeCity = p.HomeCity;
+                        ViewList.Add(obj);
+                    }
+                
 
             }
-            ViewBag.Greetings = s;
-
-            return View(pr);
-
-
-
-            //return View(viewList);
+            return View(ViewList);
         }
 
         // GET: Person/Details/5
@@ -68,117 +109,108 @@ namespace PhoonBook.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Person contact = db.Person.Find(id);
-            if (contact == null)
+
+            Person p = db.People.Find(id);
+            if (p == null)
             {
                 return HttpNotFound();
             }
-            return View(contact);
+            return View(p);
         }
+
         // GET: Person/Create
         public ActionResult Create()
         {
             return View();
         }
+       
 
         // POST: Person/Create
         [HttpPost]
-        public ActionResult Create(personviewmodel collection)
+        public ActionResult Create(PersonViewModel collection)
         {
-            
-            
             try
             {
                 // TODO: Add insert logic here
                 string id = User.Identity.GetUserId().ToString();
                 PhoneBookDbEntities db = new PhoneBookDbEntities();
                 Person obj = new Person();
-                // obj.PersonId = collection.PersonId;
                 
-                obj.AddedBy =id;
-                obj.AddedOn = collection.AddedOn;
-                obj.UpdateOn = collection.UpdateOn;
                 obj.FirstName = collection.FirstName;
+                obj.MiddleName = collection.MiddleName;
                 obj.LastName = collection.LastName;
+                obj.AddedBy = id;
+                obj.AddedOn = DateTime.Now;
+                obj.DateOfBirth = collection.DateOfBirth;
                 obj.EmailId = collection.EmailId;
+                obj.FaceBookAccountId = collection.FaceBookAccountId;
+                obj.HomeAddress = collection.HomeAddress;
+                obj.LinkedInId = collection.LinkedInId;
+                obj.ImagePath = collection.ImagePath;
+                obj.TwitterId = collection.TwitterId;
+                obj.UpdateOn = collection.UpdateOn;
                 obj.HomeCity = collection.HomeCity;
-                db.Person.Add(obj);
-               
+                db.People.Add(obj);
                 db.SaveChanges();
-               
-
-
                 return RedirectToAction("Index");
+                
             }
             catch
             {
-               
                 return View();
             }
         }
 
         // GET: Person/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
             PhoneBookDbEntities db = new PhoneBookDbEntities();
-             var person = db.Person.Where(x => x.PersonId == id);
-            List<Person> per = new List<Person>();
-
-            foreach (var i in person)
+            if (id == null)
             {
-                var pp = new Person()
-
-                {
-
-                    FirstName = i.FirstName,
-                    MiddleName = i.MiddleName,
-                    LastName = i.LastName,
-                    DateOfBirth = i.DateOfBirth,
-                    EmailId = i.EmailId,
-                    HomeAddress = i.HomeAddress,
-                    ImagePath = i.ImagePath,
-                    HomeCity = i.HomeCity,
-
-
-                };
-
-                per.Add(pp);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            ViewData["ff"] = per;
-
-
-
-            return View();
+            Person p = db.People.Find(id);
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            Person pp = new Person();
+            {
+                pp.FirstName = p.FirstName;
+                pp.MiddleName = p.MiddleName;
+                pp.LastName = p.LastName;
+                pp.HomeAddress = p.HomeAddress;
+                pp.HomeCity = p.HomeCity;
+                pp.EmailId = p.EmailId;
+                pp.DateOfBirth = p.DateOfBirth;
+            }
+            return View(pp);
         }
 
         // POST: Person/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id,personviewmodel collection)
+        public ActionResult Edit(int id, Person collection)
         {
             try
             {
-                PhoneBookDbEntities db = new PhoneBookDbEntities();
-                var persons = db.Person.Where(x=>x.PersonId==id).First();
-                persons.FirstName = collection.FirstName;
-      
-                persons.MiddleName = collection.MiddleName;
-                persons.LastName = collection.LastName;
-                persons.EmailId = collection.EmailId;
-
-                persons.DateOfBirth = collection.DateOfBirth;
-
-
-
-
-                db.SaveChanges();
-                return RedirectToAction("Index");
-
                 // TODO: Add update logic here
 
-
-
-
+                PhoneBookDbEntities db = new PhoneBookDbEntities();
+                Person obj = db.People.Find(id);
+                obj.FirstName = collection.FirstName;
+                obj.MiddleName = collection.MiddleName;
+                obj.LastName = collection.LastName;
+                obj.DateOfBirth = collection.DateOfBirth;
+                obj.EmailId = collection.EmailId;
+                obj.FaceBookAccountId = collection.FaceBookAccountId;
+                obj.HomeAddress = collection.HomeAddress;
+                obj.LinkedInId = collection.LinkedInId;
+                obj.ImagePath = collection.ImagePath;
+                obj.TwitterId = collection.TwitterId;
+                obj.UpdateOn = DateTime.Now;
+                obj.HomeCity = collection.HomeCity;
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
             catch
             {
@@ -187,53 +219,31 @@ namespace PhoonBook.Controllers
         }
 
         // GET: Person/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            //PhoneBookDbEntities db = new PhoneBookDbEntities();
-            //var person = db.Person.Where(x => x.PersonId == id);
-            //List<Person> per = new List<Person>();
-
-            //foreach (var i in person)
-            //{
-            //    var pp = new Person()
-
-            //    {
-
-            //        FirstName = i.FirstName,
-            //        MiddleName = i.MiddleName,
-            //        LastName = i.LastName,
-            //        DateOfBirth = i.DateOfBirth,
-            //        EmailId = i.EmailId,
-            //        HomeAddress = i.HomeAddress,
-            //        ImagePath = i.ImagePath,
-            //        HomeCity = i.HomeCity,
-
-
-                //};
-
-            //    per.Add(pp);
-            //}
-            return View();
+            PhoneBookDbEntities db = new PhoneBookDbEntities();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person p = db.People.Find(id);
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            return View(p);
         }
 
         // POST: Person/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id,personviewmodel collection)
+        public ActionResult Delete(int id, Person collection)
         {
             try
             {
-                PhoneBookDbEntities db = new PhoneBookDbEntities();
-                if (ModelState.IsValid)
-                {
-                    var del = (from c in db.Person
-                               where c.PersonId == id
-                               select c).FirstOrDefault();
-
-                    db.Person.Remove(del);
-                    db.SaveChanges();
-                }
-                // TODO: Add delete logic here
-
+               PhoneBookDbEntities db = new PhoneBookDbEntities();
+                Person p = db.People.Find(id);
+                db.People.Remove(p);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
